6927d79bb5fcda2df2907971ab01ceae4a80b0be
Vaibhavi Kadam <kadamvaibhavi2701@gmail.com>
2025-09-30 20:27:52 +0530

    Added dependancies

---
+++ src/main/java/org/campus/campusradarbackend/CampusRadarBackendApplication.java
@@ -0,0 +1,13 @@
+package org.campus.campusradarbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusRadarBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusRadarBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/campus/campusradarbackend/CampusRadarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.campus.campusradarbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusRadarBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
